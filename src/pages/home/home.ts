32fcf866085e9563dfa9380cfe2e22341a13197c
a <a@b.de>
2018-08-17 14:56:45 -0700

    adds code creating a pouch db

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
+console.log('this is home.ts');
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import PouchDB from 'pouchdb';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +11,13 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+        console.log('lets create a new pouch');
+        try {
+	    const pouch = new PouchDB('my pouch');
+            console.log('pouch created!', pouch);
+        } catch (err) {
+            console.log('hmmm, that did not work');
+        }
   }
 
 }
